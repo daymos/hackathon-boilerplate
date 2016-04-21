0d6373a7176bf7b4a4f59202573ae946873bd316
franzmoro <franzmoro88@gmail.com>
2016-04-21 12:18:03 +0100

    applying react-router-redux to index.js initial setup

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -3,18 +3,20 @@ import {render} from 'react-dom'
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware, compose} from 'redux'
 import promiseMiddleware from 'redux-promise'
+import { syncHistoryWithStore } from 'react-router-redux'
 import { Router, browserHistory } from 'react-router'
 
 import Routes from './routes.js'
 import reducers from './reducers'
 
-  <Router history={browserHistory} routes={Routes} />,
 const configureStore = (initialState) => {
   return createStore(reducers, initialState, compose(
     applyMiddleware(promiseMiddleware),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
   ))
 }
 const store = configureStore()
+const history = syncHistoryWithStore(browserHistory, store)
 
 render(
   <Provider store={store}>
