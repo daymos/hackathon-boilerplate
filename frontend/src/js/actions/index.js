1bfe0066411fd4635329bb05993ebf7a8c6c9dea
franzmoro <franzmoro88@gmail.com>
2016-04-21 12:19:01 +0100

    add changeTitle and toggleMenu actions

---
+++ frontend/src/js/actions/index.js
@@ -0,0 +1,11 @@
+export const changeTitle = () => {
+  return {
+    type: 'CHANGETITLE',
+    payload: 'FAC7 BOILER PLATE GOT REDUXED'
+  }
+}
+export const toggleMenu = () => {
+  return {
+    type: 'TOGGLEMENU'
+  }
+}
