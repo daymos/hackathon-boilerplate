359d496bd043072f288b0b69929d6775a3cfc7bb
franzmoro <franzmoro88@gmail.com>
2016-04-21 12:20:55 +0100

    applying redux to Home component and rendering new ChangeButton component too

--- frontend/src/js/components/Home/index.js
+++ frontend/src/js/components/Home/index.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Jumbotron } from 'react-bootstrap'
+import {connect} from 'react-redux'
 
-export default (props) => {
-  return (
-    <Jumbotron className='home'>
-      <h1>Hello Home!</h1>
-    </Jumbotron>
-  )
+import ChangeButton from '../../containers/ChangeButton'
+
+class Home extends Component {
+  render () {
+    return (
+      <Jumbotron className='home'>
+        <h1>{this.props.title}</h1>
+        <ChangeButton>This button uses Redux!</ChangeButton>
+      </Jumbotron>
+    )
+  }
 }
+
+Home.propTypes = {
+  title: React.PropTypes.string
+}
+
+const mapStateToProps = state => {
+  return {
+    title: state.changeTitle.title
+  }
+}
+
+export default connect(mapStateToProps)(Home)
