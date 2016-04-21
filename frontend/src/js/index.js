ffdbcbcfd1668af643ab3d89548c0f3091c15a28
franzmoro <franzmoro88@gmail.com>
2016-04-21 12:17:08 +0100

    applying redux setup with promises middleware to index.js

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware, compose} from 'redux'
+import promiseMiddleware from 'redux-promise'
 import { Router, browserHistory } from 'react-router'
-import Routes from './routes.js'
 
-ReactDOM.render(
+import Routes from './routes.js'
+import reducers from './reducers'
+
   <Router history={browserHistory} routes={Routes} />,
+const configureStore = (initialState) => {
+  return createStore(reducers, initialState, compose(
+    applyMiddleware(promiseMiddleware),
+  ))
+}
+const store = configureStore()
+
+render(
+  <Provider store={store}>
+  </Provider>,
   document.getElementById('app')
 )
